c4b03c0d5a7f9592980d906c7c3112a9e3769858
Airead Fan <fgh1987168@gmail.com>
2016-01-15 14:05:51 +0800

    gulpfile ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,47 @@
+var fs = require('fs');
 var gulp = require('gulp');
-var babel = require('gulp-babel');
 var browser = require('browser-sync');
+var browserify = require('browserify');
+
+var entry_files = [
+	'examples/basic/app.jsx',
+	'examples/basic-external/app.jsx',
+];
 
 gulp.task('reload', function(cb) {
-    browser.reload();
-    cb();
+	browser.reload();
+	cb();
 });
 
 gulp.task('server', function() {
-    browser.init({
-        server: {
-            baseDir: "examples",
-            index: ['index.html']
-        },
-        port: 8001,
-    });
+	browser.init({
+		server: {
+			baseDir: "examples",
+			index: ['index.html']
+		},
+		port: 8001,
+	});
 });
 
-gulp.task('jsx', function() {
-    return gulp.src('examples/**/app.jsx')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist'));
+function bundle_file(filepath) {
+	browserify(filepath)
+		.transform("babelify", {
+			presets: ["react"]
+		})
+		.bundle()
+		.pipe(fs.createWriteStream(filepath.slice(0, -1)));
+}
+
+gulp.task('browserify', function() {
+	entry_files.forEach(function(file) {
+		bundle_file(file);
+	});
+	return;
 });
 
-gulp.task('default', ['jsx', 'server'], function() {
-    gulp.watch(['examples/**/*.jsx'], ['jsx', 'reload']);
+gulp.task('default', ['browserify', 'server'], function() {
+	gulp.watch('examples/**/app.jsx', function(file) {
+		bundle_file(file.path);
+		console.log("rebundle path", file.path);
+	});
 });

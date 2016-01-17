8943a5af9381546626ac92dd8a90af2a4bf24994
Airead Fan <fgh1987168@gmail.com>
2016-01-17 22:46:15 +0800

    update

--- examples/read-real-practise/ReadRealPractise.jsx
+++ examples/read-real-practise/ReadRealPractise.jsx
@@ -4,32 +4,47 @@ import React from 'react';
 var oneLine = '一二三四五六七八九一二三四五六'.repeat(4);
 var text = [];
 for (let i = 0; i < 300; i++) {
-	text.push(oneLine);
+    text.push(oneLine);
 }
 text = text.join('\n');
 
 export default class ReadRealPractise extends React.Component {
-	static defaultProps = {
-		text: text
-	};
+    static defaultProps = {
+        lineFontNum: 40,
+        glanceFontNum: 13,
+        text: text
+    };
 
-	getLines() {
-		return this.props.text.split('\n');
-	}
+    getLines() {
+        var {text, lineFontNum} = this.props;
+        var lines = [];
+        var originLines = text.split('\n');
+        originLines.forEach(function(line) {
+            while(line.length > lineFontNum) {
+                lines.push(line.slice(0, lineFontNum));
+                line = line.slice(lineFontNum);
+            }
+            if (line.length > 0) {
+                lines.push(line);
+            }
+        });
 
-	render() {
-		var lines = this.getLines();
-		var count = 0;
-		var children = lines.map(line => {
-			count++;
-			console.log('count', count);
-			return <p key={count}>{line}</p>;
-		});
-		return (
-			<div>
-				{children}
-			</div>
-		);
-	}
+        return lines;
+    }
+
+    render() {
+        var lines = this.getLines();
+        var count = 0;
+        var children = lines.map(line => {
+            count++;
+            console.log('count', count);
+            return <p key={count}>{line}</p>;
+        });
+        return (
+            <div>
+                {children}
+            </div>
+        );
+    }
 }
 

aa1e27070f307ecee27ed8ef3bf13d9ab594ebab
Airead Fan <fgh1987168@gmail.com>
2016-03-05 13:23:02 +0800

    other:fix format

--- examples/word-think/app.jsx
+++ examples/word-think/app.jsx
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WordThink from './WordThink'
+import WordThink from './WordThink';
 
 class App extends React.Component {
     render() {
